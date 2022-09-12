bbe68c93fe1ea537e7176eaf59f75e61185d7842
ben40d9 <benpaley6@gmail.com>
2022-09-12 04:46:52 -0700

    notes

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` }); //console.lo
 console.log(process.env.USR_USERNAME);
 console.log(process.env.USR_PASSWORD);
 console.log(process.env.THE_API);
+
+//in the vscode terminal run : NODE_ENV=production node index.js
+//or NODE_ENV=development node index.js, to get the data
+//corresponding to the node environment set
+
+//
