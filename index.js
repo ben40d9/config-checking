f66228f25d9f9c3ee0a53660f5ef5c2299349d22
Joe <joe@rconjoe.com>
2022-09-12 16:13:26 -0700

    call configureEnv, env-vars route returns whatever env vars were loaded.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` }); //console.log(process.env);
 
-console.log(process.env.USR_USERNAME);
-console.log(process.env.USR_PASSWORD);
-console.log(process.env.THE_API);
+const express = require("express");
+const app = express();
+const { configureEnv } = require("./config/configureEnv");
 
 //in the vscode terminal run : NODE_ENV=production node index.js
 //or NODE_ENV=development node index.js, to get the data
 //corresponding to the node environment set
 
-//
+const { port, email } = configureEnv();
+
+app.get("/env-vars", (req, res) => {
+  res.json({
+    port,
+    email,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
