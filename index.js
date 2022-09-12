c6d0acac704538e7e569f964419bfcec48bc4662
ben40d9 <benpaley6@gmail.com>
2022-09-12 02:22:33 -0700

    now sends different data when you specify the NODE_ENV in the command line. for this just type 'NODE_ENV=production node index.js' . change process to development and there will be different values returned to the console.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(process.env.PATH);
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` }); //console.log(process.env);
+
+console.log(process.env.USR_USERNAME);
+console.log(process.env.USR_PASSWORD);
+console.log(process.env.THE_API);
