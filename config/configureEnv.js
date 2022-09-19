86b4d89c89b83327efdd2219d5ee5c6fca110d42
Joe <joe@rconjoe.com>
2022-09-19 16:47:21 -0700

    https://www.nearform.com/wp-content/uploads/2019/02/1_Co0gr64Uo5kSg89ukFD2dw-1.jpeg

--- config/configureEnv.js
+++ config/configureEnv.js
@@ -3,77 +3,52 @@ const { askFor } = require("../src/inquirer/ask");
 
 const { NODE_CONFIG_APP_PORT, NODE_CONFIG_APP_EMAIL } = process.env;
 
+const isAskEmailDisabled =
+  NODE_CONFIG_APP_EMAIL === undefined ||
+  NODE_CONFIG_APP_EMAIL === "NODE_CONFIG_APP_EMAIL";
+
 function configureEnv() {
-  configurePort()
-    .then((data) => {
-      console.log(data);
-    })
-    .then(
-      configureEmail().then((data) => {
-        console.log(data);
-      })
-    );
+  return new Promise(function (res, rej) {
+    const port = configurePort();
+
+    configureEmail().then(function (email) {
+      res({ port, email });
+    });
+
+    rej("error during env configuration");
+  });
 }
 
-// function configureEnv() {
-//   const port = configurePort();
-//   configureEmail().then(function (email) {
-//     return {
-//       port,
-//       email,
-//     };
-//   });
-// }
-//test new branch
 function configurePort() {
-  // initialize empty variable for port
   let port;
 
-  // if port is not set...
   if (
     NODE_CONFIG_APP_PORT === undefined ||
     NODE_CONFIG_APP_PORT === "EXPRESS-SERVER-PORT"
   ) {
-    // use the default. check definition of userPort!
     port = userPort;
   } else {
     port = NODE_CONFIG_APP_PORT;
   }
-
-  // // if the above "if" case isn't met, set port = to the shell env var
-  // port = NODE_CONFIG_APP_PORT || 3000;
-
-  // return port, so we can use it wherever this function is called
-  console.log(port);
   return port;
 }
 
 function configureEmail() {
-  // configure email...follows same flow, except...
-  let email;
-  // if email is undefined,
-  if (
-    NODE_CONFIG_APP_EMAIL === undefined ||
-    NODE_CONFIG_APP_EMAIL === "NODE_CONFIG_APP_EMAIL"
-  ) {
-    // run a function that will run inquirer prompt,
-    // asking user for email etc.
-    email = askFor("email");
-  } else {
-    // if condition not met, same as configurePort
-    email = NODE_CONFIG_APP_EMAIL;
-  }
+  return new Promise(function (res, rej) {
+    let email;
 
-  return console.log(email);
+    if (isAskEmailDisabled) {
+      email = NODE_CONFIG_APP_EMAIL;
+      res(email);
+    }
+
+    askFor("email").then((answer) => {
+      res(answer);
+    });
+
+    rej("error during user email configuration");
+  });
 }
-// configureEmail();
-configureEnv();
-
-// this is just so we dont have to worry about inquirer functionality  yet
-// function promptForEmail() {
-//   console.log("promptForEmail invoked");
-//   return;
-// }
 
 module.exports = {
   configureEnv,
--- src/inquirer/ask.js
+++ src/inquirer/ask.js
@@ -7,36 +7,24 @@ const questionsMap = {
   username: usernameQuestion,
 };
 
+/**
+ * @param {any} keyFromQuestionsMap - key corresponding to the question we want to ask
+ * @returns {}
+ */
 function askFor(keyFromQuestionsMap) {
-  // extract the question object we want from the map above.
-  // in the files we use it in, this allows us to do something like:
-
-  // >  const email = ask('email');
-
-  // instead of thinking so much about how to do it...import this pass that,
-  // what's the name of the question object i'm looking for, etc.
   const activeQuestion = questionsMap[keyFromQuestionsMap];
-
-  // ask the question, return the result. this should be heavily error handled.
-  prompt([activeQuestion])
-    .then(function (answers) {
-      // log and return
-      console.log(`Your answer(s) were: \n ${JSON.stringify(answers)}`);
-      return answers;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  return new Promise(function (res, rej) {
+    prompt([activeQuestion])
+      .then(function (answers) {
+        res(answers);
+      })
+      .catch(function (error) {
+        console.log(error);
+        rej(error);
+      });
+  });
 }
 
-askFor("email");
-// askFor("email").then(askFor("username"));
-
-/*
-  because the map is dealt with in here, all we have to export
-  is the question - when we use it in other files we just pass 
-  the corresponding key from 'questionsMap'
-*/
 module.exports = {
   askFor,
 };
