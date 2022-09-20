687adaff06576863f5b974a88292f1fd4cea028d
ben40d9 <benpaley6@gmail.com>
2022-09-19 23:46:14 -0700

    changes to esm. imports & exports. refactored all promises to async/await. configureEnv() & configureEmail().

--- config/configureEnv.js
+++ config/configureEnv.js
@@ -1,5 +1,6 @@
-const { port: userPort } = require("./default");
-const { askFor } = require("../src/inquirer/ask");
+import { port as userPort} from ("./default");
+
+import  askFor from "../src/inquirer/ask";
 
 const { NODE_CONFIG_APP_PORT, NODE_CONFIG_APP_EMAIL } = process.env;
 
@@ -7,16 +8,13 @@ const isAskEmailDisabled =
   NODE_CONFIG_APP_EMAIL === undefined ||
   NODE_CONFIG_APP_EMAIL === "NODE_CONFIG_APP_EMAIL";
 
-function configureEnv() {
-  return new Promise(function (res, rej) {
-    const port = configurePort();
-
-    configureEmail().then(function (email) {
-      res({ port, email });
-    });
-
-    rej("error during env configuration");
-  });
+const configureEnv = async () => {
+  const port = await configurePort();
+  const email = await configureEmail();
+  return {
+    port,
+    email
+  }
 }
 
 function configurePort() {
@@ -33,23 +31,20 @@ function configurePort() {
   return port;
 }
 
-function configureEmail() {
-  return new Promise(function (res, rej) {
-    let email;
-
-    if (isAskEmailDisabled) {
-      email = NODE_CONFIG_APP_EMAIL;
-      res(email);
+const configureEmail = async () => {
+  let email;
+  try{
+    if(isAskEmailDisabled) {
+      email = NODE_CONFIG_APP_EMAIL
+      return email
     }
-
-    askFor("email").then((answer) => {
-      res(answer);
-    });
-
-    rej("error during user email configuration");
-  });
+    const answer = await askFor("email");
+    return answer;
+  } catch (err){
+    console.log(err, "error during user email configuration")
+  }
 }
 
-module.exports = {
-  configureEnv,
+export {
+  configureEnv
 };
