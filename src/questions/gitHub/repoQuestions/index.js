7b5384aefa938675f7b6a2f76f457e4f229c48aa
ben40d9 <benpaley6@gmail.com>
2022-09-21 20:41:45 -0700

    made new q to get repo contents

--- src/questions/gitHub/repoQuestions/index.js
+++ src/questions/gitHub/repoQuestions/index.js
@@ -2,11 +2,25 @@ import { Octokit, App } from "octokit";
 
 const octokit = new Octokit({ auth: `${process.env.GH_TOKEN}` });
 
+//vars for tests, dont just want to keep putting vars
+//in .env b/c they arent private info, just repo names
+const configRepo = "config-checking";
+
 //will use later to ask username & name of repo, that user wants data from
 //variables are just hardcoded for now
-const myRepo = await octokit.request("GET /repos/{owner}/{repo}", {
-  owner: `${process.env.GH_OWNER}`,
-  repo: `${process.env.GH_REPO_NAME}`,
-});
+// const myRepo = await octokit.request("GET /repos/{owner}/{repo}", {
+//   owner: `${process.env.GH_OWNER}`,
+//   repo: `${process.env.GH_REPO_NAME}`,
+// });
+// console.log(myRepo);
 
-console.log(myRepo);
+//just another test to get a specific files' contents
+const repoContents = await octokit.request(
+  "GET /repos/{owner}/{repo}/contents/{path}",
+  {
+    owner: `${process.env.GH_OWNER}`,
+    repo: configRepo,
+    path: "index.js",
+  }
+);
+console.log(repoContents.data);
