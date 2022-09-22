1ce69f97625576ee46a0f9d211c4e82066d63e23
ben40d9 <benpaley6@gmail.com>
2022-09-21 20:23:44 -0700

    correctly pulling repos

--- src/questions/gitHub/repoQuestions/index.js
+++ src/questions/gitHub/repoQuestions/index.js
@@ -2,11 +2,11 @@ import { Octokit, App } from "octokit";
 
 const octokit = new Octokit({ auth: `${process.env.GH_TOKEN}` });
 
-// await octokit.request("GET /orgs/{org}/repos", {
-//   org: "ORG",
-// });
-
-await octokit.request("GET /repos/{owner}/{repo}", {
+//will use later to ask username & name of repo, that user wants data from
+//variables are just hardcoded for now
+const myRepo = await octokit.request("GET /repos/{owner}/{repo}", {
   owner: `${process.env.GH_OWNER}`,
-  repo: `${process.env.GH_}`,
+  repo: `${process.env.GH_REPO_NAME}`,
 });
+
+console.log(myRepo);
