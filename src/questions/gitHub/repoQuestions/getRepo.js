b82aba0eec39133f5eae8bac5a38ec97fb6ec1e4
ben40d9 <benpaley6@gmail.com>
2022-09-23 02:39:20 -0700

    import test repo var ive been using

--- src/questions/gitHub/repoQuestions/getRepo.js
+++ src/questions/gitHub/repoQuestions/getRepo.js
@@ -1,14 +1,12 @@
 import { octokit } from "../index.js";
-
-//just testing w set data of my own repo
-const configRepo = "config-checking";
+import { configRepo } from "./index.js";
 
 //will use later to ask username & name of repo, that user wants data from
 //variables are just hardcoded for now
 //repo owner & repo name is required
 const repoObj = await octokit.request("GET /repos/{owner}/{repo}", {
   owner: `${process.env.GH_OWNER}`,
-  repo: `${process.env.GH_REPO_NAME}`,
+  repo: `${configRepo}`,
 });
 
 //var that only gets repo data portion of obj
