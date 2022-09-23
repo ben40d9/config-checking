cb213e8ab6b1d1767a5faf95dd6bde8556ed421b
ben40d9 <benpaley6@gmail.com>
2022-09-23 02:41:49 -0700

    take all functions out of here and seperated them into own files. will use this file to import & export all functions, so it is easier to import in proj

--- src/questions/gitHub/repoQuestions/index.js
+++ src/questions/gitHub/repoQuestions/index.js
@@ -1,33 +1,5 @@
-import { Octokit, App } from "octokit";
-
-const octokit = new Octokit({ auth: `${process.env.GH_TOKEN}` });
-
 //vars for tests, dont just want to keep putting vars
 //in .env b/c they arent private info, just repo names
 const configRepo = "config-checking";
 
-//will use later to ask username & name of repo, that user wants data from
-//variables are just hardcoded for now
-// const myRepo = await octokit.request("GET /repos/{owner}/{repo}", {
-//   owner: `${process.env.GH_OWNER}`,
-//   repo: `${process.env.GH_REPO_NAME}`,
-// });
-// console.log(myRepo);
-
-//just another test to get a specific files' contents
-// const repoContents = await octokit.request(
-//   "GET /repos/{owner}/{repo}/contents/{path}",
-//   {
-//     owner: `${process.env.GH_OWNER}`,
-//     repo: configRepo,
-//     path: "index.js",
-//   }
-// );
-// console.log(repoContents.data);
-
-//this will list all of the public repositories for the
-//specified user
-const listReposOfUser = await octokit.request("GET /users/{username}/repos", {
-  username: `${process.env.GH_OWNER}`,
-});
-console.log(listReposOfUser);
+export { configRepo };
