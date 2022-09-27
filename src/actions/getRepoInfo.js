9a0db76fd7fd1e0e438ae1b4588d8e8e60e6586e
ben40d9 <benpaley6@gmail.com>
2022-09-27 02:09:00 -0700

    get repo is here now only

--- src/actions/getRepoInfo.js
+++ src/actions/getRepoInfo.js
@@ -1,11 +1,7 @@
 //just want to set up this simple function, returning a messy obj, to test functionality
 
 //will fix where i am setting up octokit promise, am just manually importing for now
-import { Octokit, App } from "octokit";
-
-const octokit = new Octokit({
-  auth: `${process.env.GH_TOKEN}`,
-});
+import { octokit } from "./octokit.js";
 
 import askFor from "../inquirer/ask.js";
 
@@ -24,13 +20,3 @@ export const infoFlow = async () => {
   console.log(allRepoInfo.data);
 };
 infoFlow();
-
-// //repo owner & repo name is required
-// const allRepoInfo = await octokit.request("GET /repos/{owner}/{repo}", {
-//   owner: `${process.env.GH_OWNER}`,
-//   repo: `${configRepo}`,
-// });
-
-// //var that only gets repo data portion of obj
-// const getRepo = allRepoInfo.data;
-// console.log(getRepo);
