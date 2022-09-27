0f5782956edf96dc9532afe5546a1cef57abfc81
ben40d9 <benpaley6@gmail.com>
2022-09-26 19:48:44 -0700

    notes on token q

--- src/inquirer/prompts/token.js
+++ src/inquirer/prompts/token.js
@@ -4,7 +4,10 @@ const tokenQuestion = {
   message: "What's the gitHub personal access token?",
   //validate that the answer
   validate(answer) {
-    //an empty answer would be false,
+    //an empty answer would be false, FOR NOW
+    //need to check github and see if there are token str requirements
+    //for ex like 16 characters long, only capital letters ect
+    //or if there is just a straight up check if valid octokit function
     if (!answer) {
       //in which case we return an error message
       return "Please, enter token!";
