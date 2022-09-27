7aed17a76533f4805efc27f8e97a511b523ae0e6
ben40d9 <benpaley6@gmail.com>
2022-09-27 02:15:18 -0700

    gets files contents and decodes from base64

--- src/actions/getRepoContent.js
+++ src/actions/getRepoContent.js
@@ -1,1 +1,30 @@
 //will get contents, setting up after get info is done
+import askFor from "../inquirer/ask";
+import { octokit } from "./octokit";
+
+export const repoContentFlow = async () => {
+  const owner = askFor("username");
+
+  const repo = askFor("repo");
+
+  const path = "";
+
+  //function to get a specific files' contents
+  const allRepoContents = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner: `${process.env.GH_OWNER}`,
+      repo: `${configRepo}`,
+      path: "index.js",
+    }
+  );
+
+  //set up to get .data.content, will return in Base64
+  const repoContents = allRepoContents.data.content;
+  //console.log(repoContents)
+
+  //need to set up function to translate from Base64
+  let buff = Buffer.from(repoContents, "base64");
+  let data = buff.toString();
+  console.log(data);
+};
