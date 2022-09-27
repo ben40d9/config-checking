24378b722200b213cc8cf2a92623e8087861855a
ben40d9 <benpaley6@gmail.com>
2022-09-26 18:09:48 -0700

    import / exported the token q

--- src/inquirer/prompts/index.js
+++ src/inquirer/prompts/index.js
@@ -1,5 +1,6 @@
 import { emailQuestion } from "./email.js";
 import { usernameQuestion } from "./username.js";
 import { gitHubQuestion } from "./github.js";
+import { tokenQuestion } from "./token.js";
 
-export { emailQuestion, usernameQuestion, gitHubQuestion };
+export { emailQuestion, usernameQuestion, gitHubQuestion, tokenQuestion };
