baa9ca796c303deff48116e9b62b66ff70064e19
ben40d9 <benpaley6@gmail.com>
2022-09-27 02:37:41 -0700

    mock of what will happen depending on what is chosen in menu. commented out for now

--- src/flows/launchStartupMenu.js
+++ src/flows/launchStartupMenu.js
@@ -4,41 +4,44 @@
 
 import askFor from "../inquirer/ask.js";
 
-import { infoFlow } from "../actions/getRepoInfo.js";
+// import { infoFlow } from "../actions/getRepoInfo.js";
+// import { repoContentFlow } from "../actions/getRepoContent.js";
 
 export const launchStartupMenu = async () => {
   const menuOfQuestions = await askFor("startup");
   return menuOfQuestions;
 };
 
-//just a mock of what will happen depending on what is chosen in
-export const nextStep = async () => {
-  const wait = await launchStartupMenu();
-  let theWay;
-  if (wait.startUp === "Get User Information") {
-    //run whatever you need
-    theWay = "u will get info";
-    return theWay;
-  }
-  if (wait.startUp === "Clone a Repo") {
-    //run whatever you need
-    theWay = "u will get info";
-    return theWay;
-  }
-  if (wait.startUp === "Get Repo Information") {
-    return infoFlow();
-  }
-  if (wait.startUp === "Get a Repos Content") {
-    //run
-    theWay = "u will get info";
-    return theWay;
-  }
-  if (wait.startUp === "Practice Questions") {
-    //run whatever you need
-    theWay = "u will get info";
-    return theWay;
-  }
-  return theWay;
-};
+// //just a mock of what will happen depending on what is chosen in
+// export const nextStep = async () => {
+//   const wait = await launchStartupMenu();
+//   let theWay;
+//   if (wait.startUp === "Get User Information") {
+//     //run whatever you need
+//     theWay = "You will get user info! Code coming soon!";
+//     return theWay;
+//   }
+//   if (wait.startUp === "Clone a Repo") {
+//     //run whatever you need
+//     theWay = "You will next clone a repo! Code coming soon!";
+//     return theWay;
+//   }
+//   if (wait.startUp === "Get Repo Information") {
+//     //did this one
+//     theWay = infoFlow();
+//     return theWay;
+//   }
+//   if (wait.startUp === "Get a Repos Content") {
+//     //did this one
+//     theWay = repoContentFlow();
+//     return theWay;
+//   }
+//   if (wait.startUp === "Practice Questions") {
+//     //run whatever you need
+//     theWay = "You will ppractice code! Code coming soon!";
+//     return theWay;
+//   }
+//   return theWay;
+// };
 
-nextStep();
+// nextStep();
