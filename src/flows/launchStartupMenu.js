dec0f1c285f2def96987a4f155d06b7f198cb1a4
ben40d9 <benpaley6@gmail.com>
2022-09-26 18:45:55 -0700

    made startup menu mock, not the menu i want yet just used old inq function to test functionality

--- src/flows/launchStartupMenu.js
+++ src/flows/launchStartupMenu.js
@@ -1,3 +1,10 @@
 // this file should askFor('startupMenu') and ask the user what they want to do.
 // main app functionaliies will be presented (clone a repo? practice questions? etc)
 // based on the answer from that, send into another flow or just do a simple action.
+
+import askFor from "../inquirer/ask.js";
+
+export const launchStartupMenu = async () => {
+  const menuOfQuestions = await askFor("github");
+  return menuOfQuestions;
+};
