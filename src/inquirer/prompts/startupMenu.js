929c081a98e6a50dcbb4e9395a7c8b05d141aee9
ben40d9 <benpaley6@gmail.com>
2022-09-26 21:41:15 -0700

    made startup question

---
+++ src/inquirer/prompts/startupMenu.js
@@ -0,0 +1,18 @@
+//will switch base question to "what will you be getting info on repos || users"
+const startUpQuestion = {
+  type: "list",
+  name: "startUp",
+  message: "What would you like to do?",
+  choices: [
+    "Get User Information",
+    "Clone a Repo",
+    "Get Repo Information",
+    "Get a Repos Content",
+    "Practice Questions",
+  ],
+  filter(val) {
+    return val.toLowerCase();
+  },
+};
+
+export { startUpQuestion };
